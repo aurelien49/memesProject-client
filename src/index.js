24cdb2852018d1afa5003be849f71d36d0eedf29
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-12 21:24:42 +0100

    ok pour web app

--- src/index.js
+++ src/index.js
@@ -23,7 +23,10 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+
+// Aurélien le 12/01/2023 pour l'utilisation des services workers
+//serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
