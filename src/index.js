d0dcd25f6f9c26bc141f07b415ecc4c408dda852
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-13 19:00:37 +0100

    Collapsing NavBar 47

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-
+import {MantineProvider} from '@mantine/core';
 
 // Importing the Bootstrap CSS
 //import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,11 +14,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App/>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <App/>
+            </MantineProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
-// import {MantineProvider} from '@mantine/core';
+
 // <MantineProvider>
 // withGlobalStyles withNormalizeCSS
 
