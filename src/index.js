f0f16267dccea93400d9cfeaa9fb6fb6e1422a17
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-14 12:20:17 +0100

    Collapsing NavBar 47

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Route, Link} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
