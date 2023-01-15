ce3e28a950d809c342c38fe7d8a66b460bd3fe0e
Aurelien49 <92448486+AurelienV49@users.noreply.github.com>
2023-01-15 17:37:29 +0100

    Collapsing NavBar 14-2

--- src/App.js
+++ src/App.js
@@ -149,37 +149,35 @@ class App extends React.Component {
         let _showHistoricButton = isUserLogged && (user_memes_history.length > 0);
 
         return (
-            <div id={"root"}>
-                <div className="container">
-                    <div className={"mantine-hpmcve"}>
-                        <img src={"assets/img/cocktail.453b4e0dfb1d01d3b061116811e21cdf.svg"} alt={"logo"}/>
-                        <div>
-                            <a href={"/"}>Home</a>
-                            <a href={"/"}>Coctail</a>
-                            <a href={"/"}>Favorite</a>
-                            <a href={"/"}>Random</a>
-                        </div>
-                        <div>
-                            <input className="mantine-Input-input mantine-TextInput-input mantine-1y8wd0o"
-                                   id="mantine-r0" type="text" placeholder="Search a cocktail"
-                                   aria-label="Search a cocktail" aria-invalid="false" value=""/>
+            <div className="container">
+                <div className={"mantine-hpmcve"}>
+                    <img src={"../assets/img/cocktail.453b4e0dfb1d01d3b061116811e21cdf.svg"} alt={"logo"}/>
+                    <div>
+                        <a href={"/"}>Home</a>
+                        <a href={"/"}>Cocktail</a>
+                        <a href={"/"}>Favorite</a>
+                        <a href={"/"}>Random</a>
+                    </div>
+                    <div>
+                        <input className="mantine-Input-input mantine-TextInput-input mantine-1y8wd0o"
+                               id="mantine-r0" type="text" placeholder="Search a cocktail"
+                               aria-label="Search a cocktail" aria-invalid="false" value=""/>
 
-                        </div>
                     </div>
-                    <div className={"top-menubar"}>
-                        <NavBar callbackHandleMenu={this.callbackHandleMenu} isUserLogged={isUserLogged}
-                                showHistoricButton={_showHistoricButton}></NavBar>
-                    </div>
-                    <div className={"body-content"}>
-                        {showHideHomePage &&
-                            <HomePage getUserMemesHistory={this.getUserMemesHistory} isUserLogged={isUserLogged}
-                                      user_id={this.state.user_id}/>}
-                        {showHideHistoryPage && <HistoryPage user_memes_history={user_memes_history} user_id={user_id}
-                                                             fromParentApp={this.callbackHistorical}/>}
-                        {showHideSignInPage &&
-                            <SignInPage callbackSignInSuccess={this.callbackSignInSuccess}/>}
-                        {showHideSignUpPage && <SignUpPage callbackSignUpSuccess={this.callbackSignUpSuccess}/>}
-                    </div>
+                </div>
+                <div className={"top-menubar"}>
+                    <NavBar callbackHandleMenu={this.callbackHandleMenu} isUserLogged={isUserLogged}
+                            showHistoricButton={_showHistoricButton}></NavBar>
+                </div>
+                <div className={"body-content"}>
+                    {showHideHomePage &&
+                        <HomePage getUserMemesHistory={this.getUserMemesHistory} isUserLogged={isUserLogged}
+                                  user_id={this.state.user_id}/>}
+                    {showHideHistoryPage && <HistoryPage user_memes_history={user_memes_history} user_id={user_id}
+                                                         fromParentApp={this.callbackHistorical}/>}
+                    {showHideSignInPage &&
+                        <SignInPage callbackSignInSuccess={this.callbackSignInSuccess}/>}
+                    {showHideSignUpPage && <SignUpPage callbackSignUpSuccess={this.callbackSignUpSuccess}/>}
                 </div>
             </div>
         );
